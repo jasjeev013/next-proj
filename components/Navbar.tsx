f152f23aa547ad66f75f244d31e4ae3229d01a77
Jasjeev <jasjeev99@gmail.com>
2025-02-14 01:09:01 +0530

    Add Navbar component, SearchForm, and utility functions; update layout and dependencies

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -12,7 +12,7 @@ const Navbar = async () => {
         <div className='px-5 py-3 bg-white shadow-sm font-work-sans'>
             <nav className='flex justify-between items-center'>
                 <Link href="/">
-                    <Image src="/logo.png" alt="logo" width={30} height={30} />
+                    <Image src="/logo.png" alt="logo" width={150} height={30} />
                 </Link>
                 <div className='flex items-center gap-5 text-black'>
                     {session && session?.user ? (
