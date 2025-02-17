8dfd97b81e891a2470e0cc793a03d4aa68c8fc73
Jasjeev <jasjeev99@gmail.com>
2025-02-18 00:35:10 +0530

    Enhance startup search functionality; integrate SanityLive for real-time updates and update queries for search parameters

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,17 @@
 import Image from "next/image";
 import SearchForm from "../../components/SearchForm";
 import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
-import { client } from "@/sanity/lib/client";
+
 import { STARTUPS_QUERY } from "@/lib/queries";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 
 export default async function Home({ searchParams }: {
   searchParams: Promise<{ query: string }>
 }) {
   const query = (await searchParams).query;
+  const params = {search: query || null};
 
-  const posts = await client.fetch(STARTUPS_QUERY)
+  const {data:posts} = await sanityFetch({query: STARTUPS_QUERY,params})
 
 
   return (
@@ -40,6 +42,8 @@ export default async function Home({ searchParams }: {
           )}
         </ul>
       </section>
+
+      <SanityLive />
     </>
   );
 }
